341cca9f211f74fbfe289413554b1017f8fd9372
amaresh017 <amr.chauhan11@gmail.com>
2018-01-05 18:21:39 +0530

    Seting state

--- src/App.js
+++ src/App.js
@@ -2,18 +2,109 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const names = ["Amaresh", "Jijo", "Mansoor", "Manjunath", "Bhagirathi", "Shewtah", "Thanjaya", "Vishrut"];
-const list_names = names.map((name) =>
-  <li>{name}</li>
-);
+var emp_details = [
+  {"name": "Amaresh Chauhan", "title": "Lead Developer", "dept": "Project/IT", "manager": "Vishrut"},
+  {"name": "Mansoor", "title": "Software Developer", "dept": "Project/Intg.", "manager": "Manjunath"}
+]
 
-class AssociateNames extends React.Component {
-  render(){
+class AssociateDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      employees: emp_details,
+    };
+    this.addEmployee = this.addEmployee.bind(this)
+  }
+
+  addEmployee(emp) {
+    var employee = [{"name": emp[0], "title": emp[1], "dept": emp[2], "manager": emp[3]}]
+    this.setState({
+      employees: this.state.employees.concat(employee)
+    });
+  }
+  
+  render() {
     return (
-      <div align="left">
-        <h3>Listing NHT Associate Names : </h3>
-        <ol> {list_names} </ol>
-      </div>  
+      <div>
+        <h5>Add New Employee : </h5>
+        <AddNewAssociate  addNew={this.addEmployee} />
+        <ShowList employees={this.state.employees} />
+      </div>
+    );
+  }
+}
+
+class AddNewAssociate extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      name: "",
+      title: "",
+      dept: "",
+      manager: ""
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleAddNew = this.handleAddNew.bind(this);
+  }
+  
+  handleInputChange(event) {
+    const attr_value = event.target.value;
+    const attr_name = event.target.name;
+    this.setState({
+      [attr_name]: attr_value
+    });
+  }
+
+  handleAddNew(event) {
+    this.props.addNew([this.state.name, this.state.title, this.state.dept, this.state.manager]);
+    this.setState({
+      name: "",
+      title: "",
+      dept: "",
+      manager: ""
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <label> Name:
+          <input type="text" name="name" value={this.state.name} onChange={this.handleInputChange} />
+        </label>
+        <label> Title:
+          <input type="text" name="title" value={this.state.title} onChange={this.handleInputChange} />
+        </label>
+        <label> Dept:
+          <input type="text" name="dept" value={this.state.dept} onChange={this.handleInputChange} />
+        </label>
+        <label> Manager:
+          <input type="text" name="manager" value={this.state.manager} onChange={this.handleInputChange} />
+        </label>
+        <button onClick={this.handleAddNew}> Add </button> 
+      </div>
+    );
+  }
+}
+
+class ShowList extends React.Component {
+  render() {
+    var emp_lists = this.props.employees.map((emp) =>
+      <tr>
+        <td>{emp.name}</td>
+        <td>{emp.title}</td>
+        <td>{emp.dept}</td>
+        <td>{emp.manager}</td>
+      </tr>
+    );
+    return (
+      <div>
+        <h3>Listing Associate Details</h3>
+        <table>
+          <tbody>
+            {emp_lists}
+          </tbody>  
+        </table>
+      </div>
     );
   }
 }
@@ -58,12 +149,12 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">My First React App</h1>
+          <h1 className="App-title">NHT Associates Dashboard</h1>
         </header>
         <div align="center">
           <Clock />
         </div> 
-        <AssociateNames />
+        <AssociateDetails />
       </div>
     );
   }
